426d3c241bdbc9a6108fc11c7cf1cec46d3c5f5a
miyu <miyu.mine0529@gmail.com>
2023-08-14 16:50:43 +0900

    名前のListを作成し、HelloとListを表示するように変更

--- src/main/java/com/example/demo/HelloworldController.java
+++ src/main/java/com/example/demo/HelloworldController.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloworldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/helloname")
     public String hello(){
-        return "Hello World";
+        List<String> names=List.of("yagami","ishida","takenouti","izumi","tatikawa","kido","takaishi");
+
+        return "Hello "+names;
     }
 }
